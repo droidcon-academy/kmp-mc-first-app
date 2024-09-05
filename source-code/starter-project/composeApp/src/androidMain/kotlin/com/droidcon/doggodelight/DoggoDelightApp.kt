9d11403a49f6a515c738617ac336721752f118c2
Somasundaram M <somasundaram.mahesh@hotstar.com>
2024-09-05 08:56:06 +0530

    removed starter project

--- source-code/starter-project/composeApp/src/androidMain/kotlin/com/droidcon/doggodelight/DoggoDelightApp.kt
+++
@@ -1,9 +0,0 @@
-package com.droidcon.doggodelight
-
-import android.app.Application
-
-class DoggoDelightApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
--- source-code/starter-project/composeApp/src/iosMain/kotlin/com.droidcon.doggodelight/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package com.droidcon.doggodelight
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
